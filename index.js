c7bc41806abb09ae2a3eb2123ff020c0259d16a4
Arvin  Alhissou <arvin.alhissou@gmail.com>
2020-05-21 19:28:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 // Your code goes here
+<<<<<<< HEAD
 document.addEventListener("DOMContentLoaded", function() {
   document.getElementById('text').textContent = 'This is really cool!';
 });
  
+=======
+document.addEventListener("load", function() {
+  console.log("The DOM has loaded");
+});
+ 
+console.log(
+  "JavaScript is so cool. It lets me add text to my page programmatically."
+);
+>>>>>>> 3f57ac778d25e6883916f539dcac83d1d583a6e3
